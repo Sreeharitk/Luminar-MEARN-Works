dd072c7d78ae09b169362c5280038d1d17ee5afc
Sreehari <sreeharimurali8@gmail.com>
2023-12-10 23:33:32 +0530

    slight change

--- React_Projects/demo/src/Demo1.js
+++ React_Projects/demo/src/Demo1.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Demo1.css"
 import Demo2 from "./Demo2"
               //props = {data:user,data1:100}
-const Demo1 = ({data,data1}) => { //here the {} in parameter is called destructuring curly brackets and isnide functon we dont use props anymore
+const Demo1 = ({data,data1}) => { //here the {} in parameter is called destructuring curly brackets and inside functon we dont use props anymore
   return (
     <div className="main">
         <h1>The shared data is {data} {data1}</h1>  
